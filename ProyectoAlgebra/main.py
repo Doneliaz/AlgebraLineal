9f2f678fbea5a99abaef150ff24d3769660a935b
halfanzer <147636490+halfanzer@users.noreply.github.com>
2024-06-05 22:45:58 -0600

    Update main.py

--- ProyectoAlgebra/main.py
+++ ProyectoAlgebra/main.py
@@ -1,8 +1,41 @@
 #Programa de terminal
 
-from UNIDAD1 import *
-import UNIDAD1.metgrafico2024 as metgrafico
-
+import UNIDAD1.metgrafico2024
+import UNIDAD1.newtonraph2024
+import UNIDAD3.cramer
+import UNIDAD3.GaussJordan
+import UNIDAD3.GaussSeidel
+import UNIDAD3.GaussSimple
+import UNIDAD3.Jacobi
+import UNIDAD3.LU
+import UNIDAD3.QR
+import UNIDAD4.lagrange
+import UNIDAD4.newtonpol
+import UNIDAD4.PIU
+import UNIDAD4.regresion
+import UNIDAD4.regresionmultilineal
+import UNIDAD4.regresiónlineal
+import UNIDAD5.derivadalimite
+import UNIDAD5.derivadaparcialdifadelante
+import UNIDAD5.Ecuacióndifusión
+import UNIDAD5.rectangulosint
+import UNIDAD5.rectangulovolumen
+import UNIDAD5.simpson1int
+import UNIDAD5.simpson1volumen
+import UNIDAD5.simpson2int
+import UNIDAD5.trapeciovolumen
+import UNIDAD5.trapecioint
+import UNIDAD6.Ecuacióndifusión
+import UNIDAD6.eccalor
+import UNIDAD6.eccalor3d
+import UNIDAD6.ecdiferencialeslaplace
+import UNIDAD6.heaviside
+import UNIDAD6.Heun
+import UNIDAD6.laplace2024
+import UNIDAD6.ode2024
+import UNIDAD6.rungekutta42204
+import UNIDAD6.Sistecdif
+import UNIDAD6.sistecdif2023
 
 print("Bienvenido a la terminal del programa")
 casoUnidad = int(input("Que unidad quiere inicializar?: "))
